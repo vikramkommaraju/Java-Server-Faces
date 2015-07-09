0dc88e62241e0f6556a8009f66e0428fcf31eb62
Vikram Kommaraju <vkommara@cisco.com>
2015-07-08 18:26:25 -0700

    Project layout files

---
+++ src/main/java/github/vikram/javaee/jsf/App.java
@@ -0,0 +1,13 @@
+package github.vikram.javaee.jsf;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
